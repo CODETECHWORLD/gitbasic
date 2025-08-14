a9973bbdeb30a43696c81a69b466b669f25310eb
codetechworld <parasupattu0405@gmail.com>
2025-08-14 10:21:01 +0530

    define_test_GS_in_mainMethod

--- GetterSetter.java
+++ GetterSetter.java
@@ -1,13 +1,15 @@
-public class GetterSetter {
+
+import java.util.Scanner;
+
+class DefineGetterSetter {
     private String Name;
     private int age;
 
-    
-    public GetterSetter(String name, int age) {
+    public DefineGetterSetter(String name, int age) {
         Name = name;
         this.age = age;
     }
-    
+
     public String getName() {
         return Name;
     }
@@ -21,3 +23,24 @@ public class GetterSetter {
         this.age = age;
     }   
 }
+
+public class GetterSetter{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Name");
+        String Name=sc.nextLine();
+        System.out.println("Enter the age");
+        int age=sc.nextInt();
+        DefineGetterSetter dGS=new DefineGetterSetter(Name, age);
+        GetterSetter GS=new GetterSetter();
+        System.out.println("Name in Main method : "+ dGS.getName());
+        System.out.println("Age in Method Block : "+ dGS.getAge());
+        // GS.AccessGS(dGS.getName(),dGS.getAge());
+    }
+
+    public void AccessGS(String name,int age){
+        System.out.println("Name in Method Block : "+ name);
+        System.out.println("Age in Method Block : "+ age);
+    }
+}
+
