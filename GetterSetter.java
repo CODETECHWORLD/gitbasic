3916bf3d4ce7b78f25127d15bbb3b8f83f0aeaf3
codetechworld <parasupattu0405@gmail.com>
2025-08-14 10:10:12 +0530

    GetterSetter_added

---
+++ GetterSetter.java
@@ -0,0 +1,23 @@
+public class GetterSetter {
+    private String Name;
+    private int age;
+
+    
+    public GetterSetter(String name, int age) {
+        Name = name;
+        this.age = age;
+    }
+    
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }   
+}
